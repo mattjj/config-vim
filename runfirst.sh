ff030e5fab8d837da9fb01e95deb7a678ebda10d
Matthew Johnson <mattjj@csail.mit.edu>
2012-02-21 21:43:46 -0500

    updated runfirst script not to clobber existing vimrcs

--- runfirst.sh
+++ runfirst.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -ev
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u bundles.vim +BundleInstall +q +q +q
+if [ ! -e ~/.vimrc ]
+then
+    echo "moving ~/.vimrc -> ~/.vimrc.autobak"
+    mv ~/.vimrc ~/.vimrc.autobak
+fi
 ln -sf ~/.vim/vimrc ~/.vimrc
