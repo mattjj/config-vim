b9357f53bc1e36553d887bb9eabdc509191e34b7
Matthew Johnson <mattjj@csail.mit.edu>
2012-02-22 10:01:00 -0500

    added more full path for bundles.vim file

--- runfirst.sh
+++ runfirst.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ev
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u bundles.vim +BundleInstall +q +q +q
+vim -u ~/.vim/bundles.vim +BundleInstall +q +q +q
 if [ -e ~/.vimrc ]
 then
     echo "moving ~/.vimrc -> ~/.vimrc.autobak"
