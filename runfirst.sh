9821080851989c53c85935a9e1dabc307f43e06a
Matthew Johnson <mattjj@csail.mit.edu>
2012-02-22 09:58:19 -0500

    fixed typo that inverted a condition

--- runfirst.sh
+++ runfirst.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ev
 git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u bundles.vim +BundleInstall +q +q +q
-if [ ! -e ~/.vimrc ]
+if [ -e ~/.vimrc ]
 then
     echo "moving ~/.vimrc -> ~/.vimrc.autobak"
     mv ~/.vimrc ~/.vimrc.autobak
