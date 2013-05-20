541b4913249e90a1cf7dbb706bc431ee87128089
Matthew Johnson <mattjj@csail.mit.edu>
2013-05-20 17:43:11 -0400

    removed old files

--- runfirst.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ev
-git clone http://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-vim -u ~/.vim/bundles.vim +BundleInstall +q +q +q
-if [ -e ~/.vimrc ]
-then
-    echo "moving ~/.vimrc -> ~/.vimrc.autobak"
-    mv ~/.vimrc ~/.vimrc.autobak
-fi
-ln -sf ~/.vim/vimrc ~/.vimrc
